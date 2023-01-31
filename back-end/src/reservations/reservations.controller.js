d88e245f6ff6c809f198c5dfbcf663a639aad867
Andrew Furrer <afurrer143@gmail.com>
2023-01-30 18:20:19 -0600

    format for create function

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -1,16 +1,35 @@
 const reservationService = require("./reservations.service")
-/**
- * List handler for reservation resources
- */
+const asyncErrorBoundary = require("../errors/asyncErrorBoundary")
+
+// ~~~~~~~~~~~~~~~~~~~~~ MIDDLE WARE ~~~~~~~~~~~~~~~~~~~~~
+
+const VALID_RESERVATION_PROPERTIES = [
+  "first_name",
+  "last_name",
+  "mobile_number",
+  "reservation_date",
+  "reservation_time",
+  "people",
+  "status",
+  "created_at",
+  "updated_at",
+]
+
+// ~~~~~~~~~~~~~~~~~~~~~ END POINTS ~~~~~~~~~~~~~~~~~~~~~
+
+// list returns entirety of reservation table
 async function list(req, res) {
   const data = await reservationService.list()
   res.json({ data })
 }
 
+// Create will insert a reservation from the req body data. And it is validated beforehand with middle ware to ensure its valid
 async function create(req, res){
-  
+  const data = await reservationService.create(req.body.data)
+  res.status(201).json({ data })
 }
 
 module.exports = {
-  list,
+  list: [asyncErrorBoundary(list)],
+  create: [asyncErrorBoundary(create)],
 };
--- back-end/src/reservations/reservations.service.js
+++ back-end/src/reservations/reservations.service.js
@@ -5,6 +5,15 @@ function list() {
     return knex("reservations").select("*")
 }
 
+// create takes a new reservation (which is validated in controller, inserts it and returns the new reservation) ((Technically i believe insert canbe used to insert multiple things, .first() will as it name implies only return the first thing insert but the rest will still be put in..i think))
+function create(newReservation) {
+    return knex("reservations")
+        .insert(newReservation)
+        .returning("*")
+        .first()
+}
+
 module.exports = {
     list,
+    create,
 }
